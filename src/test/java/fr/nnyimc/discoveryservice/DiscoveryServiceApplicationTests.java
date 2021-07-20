ee0bb38dbc815fe335457e2928a2af93e81cc8dd
nnyimc <nnyimc@v145.2>
2021-07-20 20:42:30 +0200

    Initial commit

---
+++ src/test/java/fr/nnyimc/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nnyimc.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
